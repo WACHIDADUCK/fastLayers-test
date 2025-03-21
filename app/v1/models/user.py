6a6caddc639596ca3233fbbe0086b39929d0b2f6
WACHIDADUCK <erguile@hotmail.com>
2025-03-21 15:23:58 +0000

    add  relacion user con user_type

--- app/v1/models/user.py
+++ app/v1/models/user.py
@@ -1,19 +1,24 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.v1.models.user_type import UserType 
 
 class BaseUser(SQLModel):
     name: str
     email: str
     profile_pic: str
     user_type_id: int
-    
-    
+
 class User(BaseUser, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    user_type_id: Optional[int] = Field(default=None, foreign_key="usertype.id")
+
+    # Relationship to UserType
+    user_type: Optional["UserType"] = Relationship(back_populates="users")
 
 class CreateUser(BaseUser):
     profile_pic: Optional[str] = None
-    pass
 
 class UpdateUser(BaseUser):
     name: Optional[str] = None
--- app/v1/models/user_type.py
+++ app/v1/models/user_type.py
@@ -1,11 +1,16 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.v1.models.user import User
 
 class BaseUserType(SQLModel):
     name: str
 
 class UserType(BaseUserType, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    
+    users: List["User"] = Relationship(back_populates="user_type")
 
 class CreateUserType(BaseUserType):
     pass
